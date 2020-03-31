5d471ffba84a8ac581f5423cca6d393a7000dbbf
Eric Brandwein <brandweineric@gmail.com>
2020-03-30 23:13:45 -0300

    Removes unused exceptions from PoliticalMapExceptions.kt

--- core/src/main/PoliticalMapExceptions.kt
+++ core/src/main/PoliticalMapExceptions.kt
@@ -3,11 +3,5 @@ import java.lang.Exception
 class NonExistentCountryException(val country: String) :
     Exception("The country $country doesn't exist.")
 
-class NonExistentContinentException(val continent: Continent) :
-    Exception("The continent $continent.name doesn't exist.")
-
-class ContinentAlreadyExistsException(val continent: Continent) :
-        Exception("The continent $continent.name already exists.")
-
 class CountryAlreadyExistsException(val country: String) :
     Exception("The country $country already exists.")
