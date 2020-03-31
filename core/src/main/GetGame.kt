eebcbb9b7b9ce4f1ad9d8e045580967076fbbdcf
nsm <smnicolas@gmail.com>
2020-03-31 17:03:36 -0300

    Add computation of image stretch

--- core/src/main/GetGame.kt
+++ core/src/main/GetGame.kt
@@ -7,16 +7,54 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 class GetGame : ApplicationAdapter() {
     lateinit var batch: SpriteBatch
     lateinit var img: Texture
+    
     override fun create() {
         batch = SpriteBatch()
         img = Texture("Sudamerica.png")
     }
 
+    fun imgFitsWidth(width: Float, heigh: Float) : Boolean {
+        return (img.getHeight().toFloat()/img.getWidth().toFloat()) >= (heigh/width)
+    }
+
+    
+    fun computeImageStretch() : Pair<Float, Float> {
+        val winWidth = Gdx.graphics.getWidth().toFloat()
+        val winHeight = Gdx.graphics.getHeight().toFloat()
+
+        val iW = img.getWidth()
+        val iH = img.getHeight()
+
+        val w = winWidth / img.getWidth()
+        val h = winHeight / img.getHeight()
+
+        var pW = 1f
+        var pH = 1f
+        
+
+        if (imgFitsWidth(winWidth, winHeight)) {
+            pW = h/w
+            
+        } else {
+            pH = w/h 
+        }
+
+        return Pair(pW * img.getWidth(), pH * img.getHeight())
+    }
+
+
+
+    
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        val stretch = computeImageStretch()
+
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        batch.draw(img, 0f, 0f,
+                   stretch.first, stretch.second, 0, 0,
+                   img.getWidth(), img.getHeight(), false, false)
         batch.end()
     }
 
