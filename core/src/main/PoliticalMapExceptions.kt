e52e9cdf6172d78ae0ab1c2599c7c088d44af2c5
Eric Brandwein <brandweineric@gmail.com>
2020-03-30 22:45:55 -0300

    Exposes Continents in PoliticalMap (#5)
    * Extracts ContinentSet from PoliticalMap
    
    * ContinentSet inherits from HashSet<Continent>
    
    * ContinentSet country existence checked with contains
    
    * Adds continents name list to PoliticalMap
    
    * Refactor reduce manager size delegating resonable responsabilities to basic types
    
    * Remove non-applicable tests due to refactor
    
    * Add test to check whether a country already exists in other continent
    
    * Adapt tests to new Continent interface
    
    * Add new tests for Continent and ContinentSet
    
    * remove of counstainsCountry was missing
    
    * Make PoliticalMap const (inmutable).
    
    * Using mutableSetOf instead of HashSet in PoliticalMap
    
    So as to not depend on the class of the MutableSet.
    
    Co-authored-by: nsm <smnicolas@gmail.com>

--- core/src/main/PoliticalMapExceptions.kt
+++ core/src/main/PoliticalMapExceptions.kt
@@ -3,8 +3,11 @@ import java.lang.Exception
 class NonExistentCountryException(val country: String) :
     Exception("The country $country doesn't exist.")
 
-class NonExistentContinentException(val continent: String) :
-    Exception("The continent $continent doesn't exist.")
+class NonExistentContinentException(val continent: Continent) :
+    Exception("The continent $continent.name doesn't exist.")
+
+class ContinentAlreadyExistsException(val continent: Continent) :
+        Exception("The continent $continent.name already exists.")
 
 class CountryAlreadyExistsException(val country: String) :
-    Exception("The country $country doesn't exist.")
+    Exception("The country $country already exists.")
