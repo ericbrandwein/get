84262ffe6ce25c67ad1576f5dc750547657a57be
Eric Brandwein <brandweineric@gmail.com>
2020-04-09 13:56:18 -0300

    Defender dice amount calculation for ClassicCombat card

--- core/src/main/situations/ClassicCombatDiceAmountCalculator.kt
+++ core/src/main/situations/ClassicCombatDiceAmountCalculator.kt
@@ -6,15 +6,19 @@ class ClassicCombatDiceAmountCalculator(
     private val attackingArmies: Int, private val defendingArmies: Int) {
 
     fun forAttacker(): Int {
-        var dice = min(MAXIMUM_DICE_AMOUNT, attackingArmies - 1)
+        var dice = boundByMaximumDiceAmount(attackingArmies - 1)
         if (shouldAddOneDieToAttacker()) {
             dice++
         }
         return dice
     }
 
+    private fun boundByMaximumDiceAmount(dice: Int) = min(MAXIMUM_DICE_AMOUNT, dice)
+
     private fun shouldAddOneDieToAttacker() =
         attackingArmies >= 2 * defendingArmies && defendingArmies >= 3
+
+    fun forDefender() = boundByMaximumDiceAmount(defendingArmies)
 
     companion object {
         private const val MAXIMUM_DICE_AMOUNT = 3
--- core/src/test/situations/ClassicCombatDiceAmountCalculatorTest.kt
+++ core/src/test/situations/ClassicCombatDiceAmountCalculatorTest.kt
@@ -4,38 +4,65 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ClassicCombatDiceAmountCalculatorTest {
-    @Test
-    fun `Attacker dice amount for an attacker with two armies should be one`() {
-        val calculator = ClassicCombatDiceAmountCalculator(2, 3)
 
-        assertEquals(1, calculator.forAttacker())
+    class ForAttacker {
+        @Test
+        fun `Dice amount for an attacker with two armies should be one`() {
+            val calculator = ClassicCombatDiceAmountCalculator(2, 3)
+
+            assertEquals(1, calculator.forAttacker())
+        }
+
+        @Test
+        fun `Dice amount for an attacker with 4 armies or less should be one less than the amount of armies`() {
+            val calculator = ClassicCombatDiceAmountCalculator(3, 1)
+
+            assertEquals(2, calculator.forAttacker())
+        }
+
+        @Test
+        fun `Dice amount for an attacker with five or more armies should be three`() {
+            val calculator = ClassicCombatDiceAmountCalculator(5, 1)
+
+            assertEquals(3, calculator.forAttacker())
+        }
+
+        @Test
+        fun `Dice amount when attacker has double the armies of the defender and defender has at least 3 armies should be 4`() {
+            val calculator = ClassicCombatDiceAmountCalculator(6, 3)
+
+            assertEquals(4, calculator.forAttacker())
+        }
+
+        @Test
+        fun `Dice amount when attacker has more than double the armies of the defender and defender has at least 3 armies should be 4`() {
+            val calculator = ClassicCombatDiceAmountCalculator(9, 4)
+
+            assertEquals(4, calculator.forAttacker())
+        }
     }
 
-    @Test
-    fun `Attacker dice amount for an attacker with 4 armies or less should be one less than the amount of armies`() {
-        val calculator = ClassicCombatDiceAmountCalculator(3, 1)
 
-        assertEquals(2, calculator.forAttacker())
-    }
+    class ForDefender {
+        @Test
+        fun `Dice amount for a defender with one army should be one`() {
+            val calculator = ClassicCombatDiceAmountCalculator(3, 1)
 
-    @Test
-    fun `Attacker dice amount for an attacker with five or more armies should be three`() {
-        val calculator = ClassicCombatDiceAmountCalculator(5, 1)
+            assertEquals(1, calculator.forDefender())
+        }
 
-        assertEquals(3, calculator.forAttacker())
-    }
+        @Test
+        fun `Dice amount for a defender with 3 armies or less should be the amount of armies`() {
+            val calculator = ClassicCombatDiceAmountCalculator(2, 3)
 
-    @Test
-    fun `Attacker dice amount when attacker has double the armies of the defender and defender has at least 3 armies should be 4`() {
-        val calculator = ClassicCombatDiceAmountCalculator(6, 3)
+            assertEquals(3, calculator.forDefender())
+        }
 
-        assertEquals(4, calculator.forAttacker())
-    }
+        @Test
+        fun `Dice amount for a defender with 4 armies or more should be 3`() {
+            val calculator = ClassicCombatDiceAmountCalculator(10, 4)
 
-    @Test
-    fun `Attacker dice amount when attacker has more than double the armies of the defender and defender has at least 3 armies should be 4`() {
-        val calculator = ClassicCombatDiceAmountCalculator(9, 4)
-
-        assertEquals(4, calculator.forAttacker())
+            assertEquals(3, calculator.forDefender())
+        }
     }
 }
