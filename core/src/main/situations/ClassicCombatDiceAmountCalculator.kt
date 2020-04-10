8ebbb2a0a7fd8184f563d4f8324cddb5d07d9f35
Eric Brandwein <brandweineric@gmail.com>
2020-04-10 14:51:27 -0300

    Inlines boundByMaximumDiceAmount in ClassicCombatDiceAmountCalculator

--- core/src/main/situations/ClassicCombatDiceAmountCalculator.kt
+++ core/src/main/situations/ClassicCombatDiceAmountCalculator.kt
@@ -6,20 +6,17 @@ class ClassicCombatDiceAmountCalculator(
     private val attackingArmies: Pos, private val defendingArmies: Pos) {
 
     fun forAttacker(): Pos {
-        var dice = boundByMaximumDiceAmount(attackingArmies - Pos(1))
+        var dice = minOf(MAXIMUM_DICE_AMOUNT, attackingArmies - Pos(1))
         if (shouldAddOneDieToAttacker()) {
             dice++
         }
         return dice
     }
 
-    private fun boundByMaximumDiceAmount(dice: Pos) =
-        minOf(MAXIMUM_DICE_AMOUNT, dice)
-
     private fun shouldAddOneDieToAttacker() =
         attackingArmies >= Pos(2) * defendingArmies && defendingArmies >= Pos(3)
 
-    fun forDefender() = boundByMaximumDiceAmount(defendingArmies)
+    fun forDefender() = minOf(MAXIMUM_DICE_AMOUNT, defendingArmies)
 
     companion object {
         private val MAXIMUM_DICE_AMOUNT = Pos(3)
