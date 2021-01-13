482e3fcb0143ae084a0094058cf90eb2e75eec2d
nsm <smnicolas@gmail.com>
2021-01-13 11:08:26 -0300

    Add debug outs

--- core/src/main/Kamchatka.kt
+++ core/src/main/Kamchatka.kt
@@ -1,6 +1,7 @@
 //import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 
@@ -10,6 +11,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.utils.viewport.Viewport
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.Pixmap
+
+
 
 
 class Kamchatka : Game() {
@@ -17,15 +22,19 @@ class Kamchatka : Game() {
     lateinit var worldmap: Texture
     lateinit var camera: OrthographicCamera
     lateinit var viewport: Viewport
+    lateinit var debug_font: BitmapFont
+    lateinit var debug_batch: SpriteBatch
 
     
     override fun create() {
+        debug_batch = SpriteBatch()
+        debug_font = BitmapFont();
         batch = SpriteBatch()
         worldmap = Texture("Sudamerica.png")
         camera = OrthographicCamera()
         viewport = FitViewport(
-            worldmap.getWidth().toFloat(),
-            worldmap.getHeight().toFloat(),
+            worldmap.width.toFloat(),
+            worldmap.height.toFloat(),
             camera
         )
     }
@@ -36,22 +45,40 @@ class Kamchatka : Game() {
 
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.setProjectionMatrix(camera.combined)
+        batch.projectionMatrix = camera.combined
 
         
         batch.begin()
         batch.draw(worldmap,
-                   -worldmap.getWidth().toFloat()/2,
-                   -worldmap.getHeight().toFloat()/2
+                   -worldmap.width.toFloat()/2,
+                   -worldmap.height.toFloat()/2
         )
+
         batch.end()
 
+        worldmap.textureData.prepare()
+        val x = Gdx.input.x
+        val y = Gdx.input.y
+
+        val color = Color(worldmap.textureData.consumePixmap().getPixel(x,  y))
+        println( "col: ${color.toString()}  x: ${x}, y: ${y}")
+/*
+        debug_batch.begin()
+        debug_font.draw(
+                debug_batch,
+                "x: ${x}, y: ${y}  col: ${color.toString()}",
+                Gdx.graphics.width.toFloat()/2,
+                Gdx.graphics.height.toFloat()/2
+        );
+        debug_batch.end()
+*/
     }
 
     override fun dispose() {
         worldmap.dispose()
         batch.dispose()
-
+        debug_batch.dispose()
+        debug_font.dispose()
     }
 
     override fun resize(width: Int, height: Int) {
