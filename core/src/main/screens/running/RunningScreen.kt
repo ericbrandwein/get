cdf61bb618ded1bbeb07e5bdbfc7e6e6e1a4a5cf
Eric Brandwein <brandweineric@gmail.com>
2021-01-25 13:23:49 -0300

    Usa AssetManager en RunningScreen

--- core/src/main/screens/running/RunningScreen.kt
+++ core/src/main/screens/running/RunningScreen.kt
@@ -2,6 +2,7 @@ package screens.running
 
 import Kamchatka
 import com.badlogic.gdx.InputProcessor
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.FitViewport
@@ -10,14 +11,23 @@ import screens.KamchatkaScreen
 
 
 class RunningScreen(game: Kamchatka) : KamchatkaScreen(game) {
-    private val worldmapTexture = Texture("mapa.png")
-    override var viewport: Viewport = FitViewport(
-        worldmapTexture.width.toFloat(),
-        worldmapTexture.height.toFloat(),
-        game.camera
-    )
-    private val stage: Stage = WorldmapStage(worldmapTexture, viewport)
-    override var inputProcessor: InputProcessor = stage
+    private val assetManager = AssetManager()
+    private val worldmapTexture: Texture
+    override val viewport: Viewport
+    private val stage: Stage
+    override val inputProcessor: InputProcessor
+
+    init {
+        assetManager.load("mapa.png", Texture::class.java)
+        worldmapTexture = assetManager.finishLoadingAsset<Texture>("mapa.png")
+        viewport = FitViewport(
+            worldmapTexture.width.toFloat(),
+            worldmapTexture.height.toFloat(),
+            game.camera
+        )
+        stage = WorldmapStage(worldmapTexture, viewport)
+        inputProcessor = stage
+    }
 
     override fun render(delta: Float) {
         super.render(delta)
@@ -28,7 +38,7 @@ class RunningScreen(game: Kamchatka) : KamchatkaScreen(game) {
 
     override fun dispose() {
         super.dispose()
-        worldmapTexture.dispose()
+        assetManager.dispose()
     }
 }
 
