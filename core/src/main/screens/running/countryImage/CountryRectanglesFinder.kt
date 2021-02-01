d63e6fbd49ea2c7b859637f5e575f5b061d57619
Eric Brandwein <brandweineric@gmail.com>
2021-02-01 14:03:37 -0300

    Agrega docs a CountryRectanglesFinder

--- core/src/main/screens/running/countryImage/CountryRectanglesFinder.kt
+++ core/src/main/screens/running/countryImage/CountryRectanglesFinder.kt
@@ -6,6 +6,10 @@ import com.badlogic.gdx.graphics.Pixmap
 import screens.running.CountryColors
 import screens.running.forEachIndexed
 
+/**
+ * Finds the minimal [IntRectangle]s that contain all pixels of each [Country] inside
+ * the [pixmap].
+ */
 fun findCountryRectangles(
     pixmap: Pixmap, countryColors: CountryColors
 ): Map<Country, IntRectangle> {
