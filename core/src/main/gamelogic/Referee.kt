880d66918f4c68b091292e57073108df0c186ab5
Eric Brandwein <brandweineric@gmail.com>
2021-02-14 15:04:01 -0300

    Extrae CountriesAreNotBorderingException a archivo aparte

--- core/src/main/gamelogic/Referee.kt
+++ core/src/main/gamelogic/Referee.kt
@@ -60,14 +60,3 @@ class Referee(
 
     fun regroup(regroupings: List<Regrouping>) = state.regroup(regroupings)
 }
-
-class CountriesAreNotBorderingException(val from: Country, val to: Country) :
-    Exception("Countries $from and $to are not bordering.")
-{
-    fun assertAreBorderingIn(politicalMap: PoliticalMap) {
-        if (!politicalMap.areBordering(from, to)) {
-            throw this
-        }
-    }
-}
-
